253f0c8183c60e3343679f78428d2e780651742e
landryPrct <landryhabonimana@outlook.com>
2022-03-22 21:03:18 +0200

    Gestion de Room dans la partie administation

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+import math
+from django.contrib.auth.models import User
 # Create your models here.
+class Room(models.Model):
+
+    name = models.CharField(max_length=40, unique=True)
+    def __str__(self):
+        return self.name
