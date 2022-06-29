16e1461ad71f9541d40d72306effdfddcbd0ee98
landryPrct <landryhabonimana@outlook.com>
2022-06-29 20:38:45 +0200

    add  jazzmin 1

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from reservations.models import Chambre, Reservation,Categorie
+from allauth.socialaccount.models import SocialToken, SocialAccount, SocialApp,EmailAddress
+
+
+admin.site.unregister(SocialToken)
+admin.site.unregister(SocialAccount)
+admin.site.unregister(SocialApp)
+admin.site.unregister(EmailAddress)
 
 
 admin.site.site_header = 'Sky Lodge Administration '
+admin.site.index_title = "Welcome Sky Lodge Administration  "
 admin.site.register(Chambre)
 admin.site.register(Reservation)
 admin.site.register(Categorie)
