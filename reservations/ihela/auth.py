03fb0e25699ea863aea22e4acd181bb7018736a1
landryPrct <landryhabonimana@outlook.com>
2022-07-05 14:51:14 +0200

    t

--- reservations/ihela/auth.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-requests.post(
-    "https://testgate.ihela.online/oAuth2/token/",
-    auth=("4sS7OWlf8pqm04j1ZDtvUrEVSZjlLwtfGUMs2XWZ", "HN7osYwSJuEOO4MEth6iNlBS8oHm7LBhC8fejkZkqDJUrvVQodKtO55bMr845kmplSlfK3nxFcEk2ryiXzs1UW1YfVP5Ed6Yw0RR6QmnwsQ7iNJfzTgeehZ2XM9mmhC3"),
-    # data={"grant_type": "E8qYLStKf6zCcLZ3KTR5iBXWXVLEXZHKmLHgQGQugmfyf8CIGhHdg3ny2BZhV6J0nNmatjNlYqLDY30GnA31y3Q3zFtUlJj8pIcjPEkwxNun2eCb5Jk72X6r5xtDmDdW"}
-)
